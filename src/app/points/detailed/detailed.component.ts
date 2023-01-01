ec3bb4cd150919b01938aef76e373d119200e76f
SalomaoS <souza95salomao@gmail.com>
2023-01-01 00:42:09 +0000

    update: detailed collector page data fetching

--- src/app/points/detailed/detailed.component.ts
+++ src/app/points/detailed/detailed.component.ts
@@ -27,21 +27,6 @@ type CollectorResponse = Omit<Collector, 'whatsapp'|'number'|'mapUrl'> & {
 	coords:string
 }
 
-const collector:CollectorResponse = {
-	id: '83d8f054-77ad-4973-a11f-da24d3431302',
-	name:'Ecoleta - Zumbi dos Palmares',
-	categories:'Eletronics, bulbs, batteries',
-	city:'Bunzaninho',
-	uf:'FFXII',
-	street:'G. Guemballa,Jardim America, 260',
-	//number:260,
-	whatsapp:555555555555,
-	email:'mailto://zimu@ecoleta.dev',
-	picture:'https://firebasestorage.googleapis.com/v0/b/ecoleta-3718e.appspot.com/o/image_1890e444-696b-40b2-9860-277e58c3b22a20221222_093946.jpg?alt=media&token=fe21cc86-6b23-4102-b78b-b6333a2b6acd',
-	//mapUrl: 'https://www.google.com/maps/@'
-	coords:'-3.0631812,-59.9370711'
-}
-
 @Component({	
   selector: 'app-detailed',
   templateUrl: './detailed.component.html',
@@ -58,8 +43,7 @@ export class DetailedComponent {
   collector:Collector|undefined
 
 	ngOnInit() {
-		this.collector = mapHttpResponse(collector)
-		/*this.paramsSubscription = this
+		this.paramsSubscription = this
 		  .route
 			.params
 			.subscribe((params) => {
@@ -69,11 +53,11 @@ export class DetailedComponent {
 					.subscribe(res => {
 						this.collector = mapHttpResponse(res)
 					})
-			})*/
+			})
 	}
 
-	/*ngOnDestroy() {
+	ngOnDestroy() {
 		this.paramsSubscription?.unsubscribe()
 		this.httpSubscription?.unsubscribe()
-	}*/
+	}
 }
